cfabecf651dcf0f88bba637b942eda47a25d776a
The-Jonsey <louis@thejonsey.com>
2020-01-18 22:43:05 +0000

    Rewriting website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          solid: "#cc3333",
+          translucent: "RGBA(204, 51, 51, 0.5)"
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
